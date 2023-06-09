f001eed210ab6bbf1b35c76edb7ac284cfd45710
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-06-09 02:30:18 +0000

    view作成

---
+++ app/controllers/public/packings_controller.rb
@@ -0,0 +1,13 @@
+class Public::PackingsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,46 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'packings/index'
+    get 'packings/new'
+    get 'packings/show'
+    get 'packings/edit'
+  end
+  namespace :public do
+    get 'schedules/index'
+    get 'schedules/new'
+    get 'schedules/show'
+    get 'schedules/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/unsubscribe'
+    get 'users/withdraw'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/new'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
 # 管理者用
 # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
