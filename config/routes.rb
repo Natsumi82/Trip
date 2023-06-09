472fd3dbe335017b9a9b38b3af2bfdbd31e4596d
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-06-09 03:34:37 +0000

    routes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+# 管理者用
+# URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   namespace :admin do
     get 'users/index'
     get 'users/show'
@@ -13,45 +18,26 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
-  namespace :public do
-    get 'packings/index'
-    get 'packings/new'
-    get 'packings/show'
-    get 'packings/edit'
-  end
-  namespace :public do
-    get 'schedules/index'
-    get 'schedules/new'
-    get 'schedules/show'
-    get 'schedules/edit'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/unsubscribe'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-# 管理者用
-# URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
 
-# ユーザー用
+
+  # ユーザー用
 # URL /customers/sign_in ..
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+  get 'about' => 'public/homes#about'
+  root 'public/homes#top'
+ scope module: :public do
+    resources :posts,only: [:index,:new,:create,:show,:edit,:update,:destroy]
+
+    resources :packings,only: [:index,:new,:create,:show,:edit,:update,:destroy]
+
+    resources :schedules,only: [:index,:new,:create,:show,:edit,:update,:destroy]
+
+    resources :users,only: [:show,:edit,:unsubscribe,:withdraw]
+
+ end
 
 end
