59e3ea0f17f55238a93131e679d4ae4dda1cf267
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-06-11 02:37:22 +0000

    投稿

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,35 @@
 class Public::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
   end
 
   def show
+     @post = Post.new(post_params)
   end
 
   def edit
+  end
+
+  def destroy
+    post = Post.new(post_params)
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post_name, :caption)
   end
 end
